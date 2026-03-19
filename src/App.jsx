c31318eb5b3ce4f45711cfbef190e33ed70dddae
Vaishu Ramalingom <vaishu.cloudriftmern@gmail.com>
2026-03-19 18:05:41 +0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 // import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Herosection";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Herosection />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,37 @@
 import React from "react";
+import {
+  FaHome,
+  FaInfoCircle,
+  FaServicestack,
+  FaEnvelope,
+} from "react-icons/fa";
 function Navbar() {
   return (
-    <div className="fixed top-0 left-0 w-full flex justify-between items-center  px-6 py-3 text-blue-500 font-bold bg-grey">
-      <div>
+    <nav className="p-4 fixed top-0 left-0 w-full font-bold bg-gray-400  text-black">
+      <div className="flex items-center justify-between">
         <h1>🚀My Website</h1>
-      </div>
-      <div>
-        <ul className="flex gap-6">
+        <ul className="flex text-black">
           <li>
-            <a href="#">Home</a>
+            <a href="#" className="flex items-center gap-2 p-4 hover:text-blue">
+              <FaHome />
+              Home
+            </a>
           </li>
           <li>
-            <a href="#">About</a>
+            <a href="#" className="flex items-center gap-2 p-4">
+              <FaInfoCircle />
+              About
+            </a>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <a href="#" className="flex items-center gap-2 p-4">
+              <FaEnvelope />
+              Contact
+            </a>
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 }
 export default Navbar;
